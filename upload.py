3b14bde958a8abd91a6fe6b93b8d9e87c3ab348c
ledleledle <leontodsky@gmail.com>
2019-04-03 13:31:21 +0700

    gallery

--- upload.py
+++ upload.py
@@ -1,12 +1,9 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_from_directory
 from PIL import Image
 import time
 
 app = Flask(__name__)
-
-#UPLOAD_FOLDER = os.path.basename('img')
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
@@ -18,12 +15,18 @@ def upload_file():
     img = Image.open(file).convert('L')
     timestr = time.strftime("%Y%m%d-%H%M%S")
     img.save('img/' + timestr + 'grey.png')
-    #f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    
-    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
-    #file.save(f)
-
+ 
     return redirect("/", code=302)
     
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("img", filename)
+
+@app.route('/gallery')
+def get_gallery():
+    image_names = os.listdir('img')
+    print(image_names)
+    return render_template("gallery.html", image_names=image_names)
+
 if __name__ == "__main__":
     app.run(debug=True)
